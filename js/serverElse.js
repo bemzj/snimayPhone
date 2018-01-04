3e9f15ba1af15a37c8d39b00f11e526c8684b69b
bemzj <qiuzijia520@qq.com>
2018-01-04 18:16:28 +0800

    2018141816

---
+++ js/serverElse.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('.mbRight').height($('.mbLeft').height());
+	//开启滑动动画
+	if(!(/msie [6|7|8|9]/i.test(navigator.userAgent))) {
+		(function() {
+			window.scrollReveal = new scrollReveal({
+				reset: true
+			});
+		})();
+	};
+});
