59a03c9b52463f8934e476d7e815d23899fb7dc4
bemzj <qiuzijia520@qq.com>
2018-01-06 15:28:25 +0800

    2011816

--- js/center.js
+++ js/center.js
@@ -1,3 +1,4 @@
 $(function(){
-	
+	$('.selectChild').width($(window).width()/3.5);
+	$('.home .selectBox').width($(window).width()/3.5*$('.home .selectBox').children('.selectChild').length+2*($('.home .selectBox').children('.selectChild').length-1));
 });
