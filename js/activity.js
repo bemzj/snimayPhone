6533716284be952286103e634adc4c6b1d856951
bemzj <qiuzijia520@qq.com>
2018-03-08 18:31:14 +0800

    xx

---
+++ js/activity.js
@@ -0,0 +1,19 @@
+$(function(){
+	!function () {
+        var $target = $('#city');
+
+        $target.citySelect();
+
+        $target.on('click', function (event) {
+            event.stopPropagation();
+            $target.citySelect('open');
+        });
+
+        $target.on('done.ydui.cityselect', function (ret) {
+            $(this).val(ret.provance + ' ' + ret.city + ' ' + ret.area);
+        });
+    }();
+    $('#city').on('click',function(){
+    	$(this).blur();
+    });
+});
