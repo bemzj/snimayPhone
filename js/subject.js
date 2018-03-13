d2aac27c5b7baa711fa689fcf051b8722d5b219e
bemzj <qiuzijia520@qq.com>
2018-03-13 14:16:22 +0800

    xx

--- js/subject.js
+++ js/subject.js
@@ -26,6 +26,12 @@ $(function(){
 	setTimeout(function(){
 		$('.swiperBox').width($('.sBtn').width()*4+parseInt($('.sBtn').css('margin-right'))*4);
 	},100);
+	
+	setTimeout(function(){
+		$('.ptbBox .floatl').width($('.ptwoBottom').width()*0.3);
+		$('.ptbBox .floatl').css('margin-right',$('.ptwoBottom').width()*0.05);
+		$('.ptbBox').width($('.ptwoBottom').width()*0.3*4+$('.ptwoBottom').width()*0.05*3);
+	},100);
 	$('.swiperBox').find('.sBtn').on('click',function(){
 		var index = $(this).index();
 		$(this).siblings('.sBtn').find('input').attr('src','img/sub03.png');
@@ -34,4 +40,34 @@ $(function(){
 		$(this).find('p').css('color','white');
 		swiperSupport.slideTo(index);
 	});
+	var swiperPart = new Swiper('.swiper-container-part02', {
+		pagination : '.swiper-pagination',
+		paginationClickable :true,
+		onInit: function(swiper){
+		},
+		onSlideChangeEnd: function(swiper){
+			
+			$('.ptbBox').find('.floatl').eq(swiper.activeIndex).addClass('showf');
+			$('.ptbBox').find('.floatl').eq(swiper.activeIndex).siblings('.floatl').removeClass('showf');
+			if(swiper.activeIndex==3)
+		     {
+		      	var left = $('.ptbBox').width()-$('.ptwoBottom').width();
+		      	$('.ptwoBottom').animate({  
+		            scrollLeft: left
+		        }, 400); 
+		      }else{
+		      	$('.ptwoBottom').animate({  
+		            scrollLeft: 0
+		        }, 400); 
+		      }
+		}
+	});
+	
+	$('.ptwoBottom .ptbBox .floatl').on('click',function(){
+		var index = $(this).index();
+		$(this).addClass('showf');
+		$(this).siblings('.floatl').removeClass('showf');
+		swiperPart.slideTo(index);
+	});
+	
 });
